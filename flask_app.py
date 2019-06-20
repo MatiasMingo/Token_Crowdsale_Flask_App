e757adfa5148c63180be030de221f4807d3a9d75
Unknomads <mamingo@uc.cl>
2019-06-19 20:09:18 -0400

    Basic login template written

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,27 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Secretkeyyy'
 Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
 @app.route("/")
 def index():
     return render_template("homepage.html", name="homepage")
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login')
 def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 @app.route('/user/<username>')
 def profile(username):
